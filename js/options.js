844044cb330fc4894fc078a6adadb760d1f78fa9
Tautvidas <t.9b5as@gmail.com>
2012-09-22 13:42:19 +0300

    Cleanup and JSHint compliance

--- js/options.js
+++ js/options.js
@@ -1,63 +1,52 @@
-$(document).ready(function(){
-	restore_options();
-	
-	$("input[name=rule]").change(function(){
-		
-		if($("input[name=rule]:checked").val() == "custom")
-		{
-			$("#customForm").show("fast");
-		} else {
-			$("#customForm").hide("fast");
-		}
-	});
-	
-	$("#apply").click(function(){
-		var backgroundPage = chrome.extension.getBackgroundPage();
-		backgroundPage.settings.rule = $("input[name=rule]:checked").val();
-		backgroundPage.settings.workTime = $("#workTime").val();
-		backgroundPage.settings.breakTime = $("#breakTime").val();
-		backgroundPage.settings.playSound = $("input[name=playSound]:checked").val();
-		backgroundPage.updateSettings();
-		$("#status").hide("fast");
-		$("#status").html('<font color="green">Options were successfuly applied.</font>');
-		$("#status").show("fast");
-		setTimeout(function(){
-			$("#status").hide("fast");
-		}, 2000);
-    backgroundPage._gaq.push(['_trackEvent', 'settings:' + backgroundPage.settings.rule + ' ' + backgroundPage.settings.workTime + ' ' + backgroundPage.settings.breakTime + ' ' + backgroundPage.settings.playSound, 'updated']);
-		
-	});
+$(document).ready(function () {
+    restore_options();
+
+    $("input[name=rule]").change(function () {
+
+        if ($("input[name=rule]:checked").val() === "custom") {
+            $("#customForm").show("fast");
+        } else {
+            $("#customForm").hide("fast");
+        }
+    });
+
+    $("#apply").click(function () {
+        var backgroundPage = chrome.extension.getBackgroundPage();
+        backgroundPage.settings.rule = $("input[name=rule]:checked").val();
+        backgroundPage.settings.workTime = $("#workTime").val();
+        backgroundPage.settings.breakTime = $("#breakTime").val();
+        backgroundPage.settings.playSound = $("input[name=playSound]:checked").val();
+        backgroundPage.updateSettings();
+        $("#status").hide("fast");
+        $("#status").html('<font color="green">Options were successfuly applied.</font>');
+        $("#status").show("fast");
+        setTimeout(function () {
+            $("#status").hide("fast");
+        }, 2000);
+
+    });
 });
 
 // Restores select box state to saved value from localStorage.
 function restore_options() {
-	var backgroundPage = chrome.extension.getBackgroundPage();
-	var rule = backgroundPage.settings.rule;
-	var playSound = backgroundPage.settings.playSound;
-	
-    if (!rule) {
-		//return;
-	} else {
-		$("input[name=rule][value=" + rule + "]").attr('checked', true);  
-		if(rule == "custom")
-		{
-			$("#customForm").show("fast");
-		}
-	}
-	if (playSound == null) {
-		//return;
-	} else {
-		$("input[name=playSound][value=" + playSound + "]").attr('checked', true);  
-	}
-	if (backgroundPage.settings.breakTime == null) {
-		//return;
-	} else {
-		$("#breakTime").val(backgroundPage.settings.breakTime);  
-	}
-	if (backgroundPage.settings.workTime == null) {
-		//return;
-	} else {
-		$("#workTime").val(backgroundPage.settings.workTime);  
-	}
-	
+    var backgroundPage = chrome.extension.getBackgroundPage(),
+        rule = backgroundPage.settings.rule,
+        playSound = backgroundPage.settings.playSound;
+
+    if (rule) {
+        $("input[name=rule][value=" + rule + "]").attr('checked', true);
+        if (rule === "custom") {
+            $("#customForm").show("fast");
+        }
+    }
+    if (playSound) {
+        $("input[name=playSound][value=" + playSound + "]").attr('checked', true);
+    }
+    if (backgroundPage.settings.breakTime) {
+        $("#breakTime").val(backgroundPage.settings.breakTime);
+    }
+    if (backgroundPage.settings.workTime) {
+        $("#workTime").val(backgroundPage.settings.workTime);
+    }
+
 }
--- js/popup.js
+++ js/popup.js
@@ -1,39 +1,43 @@
 var backgroundPage = chrome.extension.getBackgroundPage();
-$(document).ready(function(){
-	drawTimer();
-  $('.iconReset').click(function() {
-    backgroundPage._gaq.push(['_trackEvent', 'button-reset', 'clicked']);
-  });
-  $('.iconInfo').click(function() {
-    backgroundPage._gaq.push(['_trackEvent', 'button-statistics', 'clicked']);
-  });
-  $('.iconPreferences').click(function() {
-    backgroundPage._gaq.push(['_trackEvent', 'button-options', 'clicked']);
-  });
+$(document).ready(function () {
+    drawTimer();
+    $('.iconReset').bind('click', reset_timer);
+    $('.iconStop').bind('click', stop_timer);
 });
+
 function drawTimer() {
-	var nextBreak = backgroundPage.nextBreak;
-	var timer = $("#timer");
-	var minutes, seconds;
-	timerLeft = Math.round((nextBreak - backgroundPage.getCurrentTimestamp()) / 1000);
-	if(timerLeft >= 0)
-	{
-		seconds = timerLeft % 60;						
-		minutes = (timerLeft - seconds) / 60;	
-		if(seconds < 10) 
-		{
-			seconds = '0' + seconds;
-		}
-		if(minutes < 10)
-		{
-			minutes = '0' + minutes;
-		}
-		timer.html(minutes + ':' + seconds);
-			
-	}								
-	setTimeout(drawTimer, 1000);
+    "use strict";
+    var nextBreak = backgroundPage.nextBreak,
+        timer = $("#timer"),
+        minutes,
+        seconds;
+    timerLeft = Math.round((nextBreak - backgroundPage.getCurrentTimestamp()) / 1000);
+    if (timerLeft >= 0) {
+        seconds = timerLeft % 60;
+        minutes = (timerLeft - seconds) / 60;
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+        timer.html(minutes + ':' + seconds);
+
+    }
+    setTimeout(drawTimer, 1000);
 }
 
 function reset_timer() {
-	backgroundPage.resetTimer();
+    "use strict";
+    backgroundPage.resetTimer();
+}
+
+function stop_timer() {
+    "use strict";
+    backgroundPage.stop_timer();
+}
+
+function start_timer() {
+    "use strict";
+    backgroundPage.resetTimer();
 }
